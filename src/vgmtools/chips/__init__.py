0b49a6d37e95940cd80af09dd003a4aac4222253
Alexander Shokhin <std282@icloud.com>
2025-07-31 00:51:36 +0300

    changed filename and class name for YM2612 FM handler

--- src/vgmtools/chips/__init__.py
+++ src/vgmtools/chips/__init__.py
@@ -1,5 +1,5 @@
 from .sn76489 import Chip as SN76489
-from .ym2612 import Chip as YM2612
+from .ym2612_fm import Chip as YM2612FM
 from .ym2612_dac import Chip as YM2612DAC
 from vgmtools.vgm import Command as VGMCommand
 from typing import Protocol
@@ -22,8 +22,8 @@ class UnknownChip(Exception) -> SomeChip:
 
 def chip(name: str) -> Chip:
     match name.lower():
-        case 'ym2612':
-            return YM2612()
+        case 'ym2612/fm':
+            return YM2612FM()
         case 'ym2612/dac':
             return YM2612DAC()
         case 'sn76489':
@@ -36,8 +36,8 @@ def system(name: str) -> System:
     cs: Chipset
     match name.lower():
         case 'genesis':
-            return tuple(map(chip, ('ym2612', 'ym2612/dac', 'sn76489')))
-        case 'ym2612' | 'sn76489' | 'ym2612/dac':
+            return tuple(map(chip, ('ym2612/fm', 'ym2612/dac', 'sn76489')))
+        case 'ym2612/fm' | 'sn76489' | 'ym2612/dac':
             return (chip(name),)
         case _:
             raise UnknownChip(name)
